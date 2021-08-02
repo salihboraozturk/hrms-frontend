fc85a4282e82f27de6ed5de9797479f9a25e4efc
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-08-02 14:59:53 +0300

    Version 1.0.5

--- src/pages/JobPostingDetail.jsx
+++ src/pages/JobPostingDetail.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Card, Icon, Image, Item, Label, Container } from "semantic-ui-react";
+import {
+  Button,
+  Card,
+  Icon,
+  Image,
+  Item,
+  Label,
+  Container,
+} from "semantic-ui-react";
 import JobPostingService from "../services/jobPostingService";
 import {
   Divider,
@@ -26,42 +34,48 @@ export default function JobPostingDetail() {
   return (
     <div>
       <Container text style={{ marginTop: "1em" }}>
-        <Image
-          centered
-          size="medium"
-          src={jobPosting.employer?.companyLogo}
-          style={{ marginTop: "2em" }}
-        />
-        <Header as="h1">
-          {jobPosting.jobPosition?.positionName}
-          <Header.Subheader>
-            {jobPosting.employer?.companyName}
-          </Header.Subheader>
-        </Header>
-        <Divider></Divider>
-        <h2>İlan Detayları</h2>
-        <p>{jobPosting.jobDescription}</p>
-        <p>
-          <strong>Maaş Skalası:</strong>
-          <Label as="a">₺{jobPosting.minSalary}</Label>-
-          <Label as="a">₺{jobPosting.maxSalary}</Label>
-        </p>
-        <p>
-          <Label color="green" as="a">
-            Açık Pozisyon Sayısı
-            <Label.Detail>{jobPosting.numberOfOpenPosition}</Label.Detail>
-          </Label>
-          <Label as="a" color="teal" image>
-            Çalışma Türü
-            <Label.Detail>{jobPosting.jobType?.jobTypeName}</Label.Detail>
-          </Label>
-          <Label as="a" color="yellow" image>
-            Çalışma Şekli
-            <Label.Detail>
-              {jobPosting.workingTime?.workingTimeName}
-            </Label.Detail>
-          </Label>
-        </p>
+        <Grid>
+          <Grid.Column>
+            <Image
+              size="medium"
+              label={{
+                as: "h1",
+                color: "black",
+                content: jobPosting.listingDate,
+                ribbon: true,
+              }}
+              src={jobPosting.employer?.companyLogo}
+            />
+            <Header as="h1">{jobPosting.jobPosition?.positionName}</Header>
+            <Header as="h4">
+              <Icon name="map marker" />
+              {jobPosting.employer?.companyName}-{jobPosting.city?.cityName}
+            </Header>
+            <p>{jobPosting.jobDescription}</p>
+            <p>
+              <strong>Maaş Skalası:</strong>
+              <Label as="a">₺{jobPosting.minSalary}</Label>-
+              <Label as="a">₺{jobPosting.maxSalary}</Label>
+            </p>
+            <p>
+              <Label color="teal" as="a">
+                Açık Pozisyon Sayısı
+                <Label.Detail>{jobPosting.numberOfOpenPosition}</Label.Detail>
+              </Label>
+              <Label as="a" color="red" image>
+                Çalışma Türü
+                <Label.Detail>{jobPosting.jobType?.jobTypeName}</Label.Detail>
+              </Label>
+              <Label as="a" color="yellow" image>
+                Çalışma Şekli
+                <Label.Detail>
+                  {jobPosting.workingTime?.workingTimeName}
+                </Label.Detail>
+              </Label>
+            </p>
+            <Button positive>Başvur</Button>
+          </Grid.Column>
+        </Grid>
       </Container>
     </div>
   );
